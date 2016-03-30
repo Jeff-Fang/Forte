7a80ede1c0f932788443aacf92a4a2f2e6c4978f
Jeff Fang <all2jeff@gmail.com>
2016-03-30 21:15:27 +0800

    Make the future glossary view use the proper Data Model.

--- Forte/SearchViewController.swift
+++ Forte/SearchViewController.swift
@@ -13,10 +13,10 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
-//    var glossary:[GlossaryItem] = []
+    var glossary:[GlossaryItem] = []
     
-    var searchResults = [String]()
-//    var hasSearched = false
+    var searchResults:[GlossaryItem] = []
+    var hasSearched = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,15 +24,15 @@ class SearchViewController: UIViewController {
         tableView.contentInset.bottom = 66
         
         // Load menu items from database
-//        if let managedObjectContext = (UIApplication.sharedApplication().delegate as? AppDelegate)?.managedObjectContext {
-//            let fetchRequest = NSFetchRequest(entityName: "GlossaryItem")
-//            do {
-//                glossary = try managedObjectContext.executeFetchRequest(fetchRequest) as! [GlossaryItem]
-//            } catch {
-//                print("Failed to retrieve record")
-//                print(error)
-//            }
-//        }
+        if let managedObjectContext = (UIApplication.sharedApplication().delegate as? AppDelegate)?.managedObjectContext {
+            let fetchRequest = NSFetchRequest(entityName: "GlossaryItem")
+            do {
+                glossary = try managedObjectContext.executeFetchRequest(fetchRequest) as! [GlossaryItem]
+            } catch {
+                print("Failed to retrieve record")
+                print(error)
+            }
+        }
         
         
     }
@@ -44,13 +44,33 @@ class SearchViewController: UIViewController {
 
 extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(searchBar: UISearchBar) {
-        print("Search Bar Search Button Clicked!")
-        searchResults = [String]()
-        for i in 0...2 {
-            searchResults.append(String(format: "Fake Result %d for '%@'", i,
-                searchBar.text!))
-        }
+        searchBar.resignFirstResponder()
         tableView.reloadData()
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == "" {
+            hasSearched = false
+        } else {
+            hasSearched = true
+        }
+        
+        searchResults = [GlossaryItem]()
+        
+        if let searchText = searchBar.text {
+            print(searchText)
+            print("searchResults.count = \(searchResults.count)")
+            filterContentForSearchText(searchText)
+            tableView.reloadData()
+        }
+        
+    }
+    
+    func filterContentForSearchText(searchText: String) {
+        searchResults = glossary.filter({(glossaryItem:GlossaryItem) -> Bool in
+            let nameMatch = glossaryItem.term!.rangeOfString(searchText, options:NSStringCompareOptions.CaseInsensitiveSearch)
+            return nameMatch != nil
+        })
     }
     
     func positionForBar(bar: UIBarPositioning) -> UIBarPosition {
@@ -60,20 +80,34 @@ extension SearchViewController: UISearchBarDelegate {
 
 extension SearchViewController: UITableViewDataSource {
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return searchResults.count
+        if hasSearched {
+            return searchResults.count
+        } else {
+            return glossary.count
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
         let cellIdentifier = "SearchResultCell"
         var cell: UITableViewCell! = tableView.dequeueReusableCellWithIdentifier(cellIdentifier)
         if cell == nil {
             cell = UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
         }
-        cell.textLabel!.text = searchResults[indexPath.row]
+        
+        if hasSearched {
+            cell.textLabel!.text = searchResults[indexPath.row].term
+        } else {
+            cell.textLabel!.text = glossary[indexPath.row].term
+        }
+        
         return cell
     }
 }
 
 extension SearchViewController: UITableViewDelegate {
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
     
 }
