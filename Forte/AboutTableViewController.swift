3aeee2908bb2202b88987787fe3659e205844839
Jeff Fang <all2jeff@gmail.com>
2016-05-01 23:54:54 +0800

    optimize the extensibility of AboutVC

--- Forte/AboutTableViewController.swift
+++ Forte/AboutTableViewController.swift
@@ -10,15 +10,60 @@ import UIKit
 import SafariServices
 import MessageUI
 
+struct MenuItem {
+    var text: String
+    var row: Int
+}
+
+struct Section {
+    var items: [MenuItem]
+    var name: String
+    var sectionNumber: Int
+}
+
+// MARK: - Configure Menu Items
+
+let wikiPage = MenuItem(text:"Music Glossary on Wikipedia", row: 0)
+let aboutForte: [MenuItem] = [wikiPage]
+let aboutSection = Section(items: aboutForte, name: "About", sectionNumber:0)
+
+let rate = MenuItem(text: "Rate Forte on App Store", row: 0)
+let sendFeedback = MenuItem(text: "Tell me your feedback", row: 1)
+let reportProblem = MenuItem(text: "Report a problem", row: 2)
+let feedback: [MenuItem] = [rate, sendFeedback, reportProblem]
+let feedbackSection = Section(items: feedback, name: "Leave Feedback", sectionNumber: 1)
+
+let myTwitter = MenuItem(text: "Twitter", row: 0)
+let myWeibo = MenuItem(text: "Weibo", row: 1)
+let followMe: [MenuItem] = [myTwitter, myWeibo]
+let followMeSection = Section(items: followMe, name:"Follow Me", sectionNumber: 2)
+
+let aboutPageMenu: [Section] = [aboutSection, feedbackSection, followMeSection]
+
+func aboutPageMenuItemForIndexPath(index: NSIndexPath) -> MenuItem? {
+    for sec in aboutPageMenu {
+        if sec.sectionNumber == index.section {
+            for item in sec.items {
+                if item.row == index.row {
+                    return item
+                }
+            }
+        }
+    }
+    return nil
+}
+
+//MARK: - ViewController
+
 class AboutTableViewController: UITableViewController {
 
     // MARK: - Class Properties
 
-    var sectionTitles = ["About","Leave Feedback","Follow Me"]
-    var sectionContent = [["Music Glossary on Wikipedia"],
-                          ["Rate Forte on App Store", "Tell me your feedback", "Report a problem"],
-                          ["Twitter", "Weibo"]]
-    var links = ["https://twitter.com/all2jeff","https://weibo.com/all2jeff"]
+    let links = [myTwitter.text: "https://twitter.com/all2jeff",
+                 myWeibo.text: "https://weibo.com/all2jeff",
+                 wikiPage.text: "https://en.wikipedia.org/wiki/Glossary_of_musical_terminology",
+                 rate.text: "http://www.apple.com/itunes/charts/paid-apps/"]
+
 
     // MARK: - Class Settings
 
@@ -35,66 +80,78 @@ class AboutTableViewController: UITableViewController {
     // MARK: - UITableViewDataSourceDelegate
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 3
+        return aboutPageMenu.count
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch section {
-        case 0:
-            return 1
-        case 1:
-            return 3
-        case 2:
-            return 2
-        default:
-            return 0
+        var number = 0
+        for sct in aboutPageMenu {
+            if sct.sectionNumber == section {
+                number = sct.items.count
+            }
         }
+        return number
     }
     
     override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sectionTitles[section]
+        var sctTitle: String?
+        for sct in aboutPageMenu {
+            if sct.sectionNumber == section {
+                sctTitle = sct.name
+            }
+        }
+        return sctTitle
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
-        cell.textLabel?.text = sectionContent[indexPath.section][indexPath.row]
-
+        if let itemInfo = aboutPageMenuItemForIndexPath(indexPath) {
+            cell.textLabel?.text = itemInfo.text
+        }
         return cell
     }
     
     // MARK: - UITableViewDelegate
 
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        switch indexPath.section{
-        case 0:
-            if let url = NSURL(string: "https://en.wikipedia.org/wiki/Glossary_of_musical_terminology") {
-                let safariController = SFSafariViewController(URL: url, entersReaderIfAvailable: false)
-                presentViewController(safariController, animated: true, completion: nil)
-            }
-        
-        case 1:
-            if indexPath.row == 0 {
-                if let url = NSURL(string:"http://www.apple.com/itunes/charts/paid-apps/") {
-                    UIApplication.sharedApplication().openURL(url)
+        if let cellItem = aboutPageMenuItemForIndexPath(indexPath) {
+            switch cellItem.text{
+            case wikiPage.text:
+                if let url = NSURL(string: links[wikiPage.text]!) {
+                    let safariController = SFSafariViewController(URL: url, entersReaderIfAvailable: false)
+                    presentViewController(safariController, animated: true, completion: nil)
                 }
-            } else {
-                showEmailForRow(indexPath.row)
-            }
+                
+            case rate.text:
+                    if let url = NSURL(string:links[rate.text]!) {
+                        UIApplication.sharedApplication().openURL(url)
+                }
             
-        case 2:
-            if let url = NSURL(string: links[indexPath.row]) {
-                let safariController = SFSafariViewController(URL: url, entersReaderIfAvailable: false)
-                presentViewController(safariController, animated: true, completion: nil)
-            }
+            case sendFeedback.text:
+                showEmailForKey(sendFeedback.text)
             
-        default:
-            break
+            case reportProblem.text:
+                showEmailForKey(sendFeedback.text)
+                
+            case myTwitter.text:
+                if let url = NSURL(string: links[myTwitter.text]!) {
+                    let safariController = SFSafariViewController(URL: url, entersReaderIfAvailable: false)
+                    presentViewController(safariController, animated: true, completion: nil)
+                }
+            case myWeibo.text:
+                if let url = NSURL(string: links[myWeibo.text]!) {
+                    let safariController = SFSafariViewController(URL: url, entersReaderIfAvailable: false)
+                    presentViewController(safariController, animated: true, completion: nil)
+                }
+                
+            default:
+                break
+            }
         }
-        
         tableView.deselectRowAtIndexPath(indexPath, animated: false)
     }
     
-    func showEmailForRow(row:Int) {
+    func showEmailForKey(key: String) {
         print("*** sending mails .....")
         guard MFMailComposeViewController.canSendMail() else {
             return
@@ -103,9 +160,9 @@ class AboutTableViewController: UITableViewController {
         let toRecipients = ["all2jeff@gmail.com"]
         var emailTitle = ""
         
-        if row == 1 {
+        if key == sendFeedback.text {
             emailTitle = "Feedback of Forte"
-        } else if row == 2 {
+        } else if key == reportProblem.text {
             emailTitle = "Problem of Forte"
         }
         
